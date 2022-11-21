a2446037457397066dd5bd676dc3c39691c5adf1
CHARITAH <102732524+CHARITAH@users.noreply.github.com>
2022-11-21 20:59:23 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -24,3 +24,21 @@ function roomadding(){
       window.location = "kwitter_page.html";
       
 } 
+
+function getData() {firebase.database().ref("/").on('value',
+function(snapshot) {document.getElementById("output").innerHTML =
+"";snapshot.forEach(function(childSnapshot) {childKey = childSnapshot.key;
+Room_names = childKey;
+//Start code
+ console.log("Room Name - " + Room_names);
+      row = "<div class = 'room_name' id = "+Room_names+" onclick = 'redirectroom(this.id)'>#"+Room_names +"</div><hr>";
+      document.getElementById("output").innerHTML += row;
+//End code
+});});}
+getData();
+
+function redirectroom(name){
+      console.log(name);
+      localStorage.setItem("room_name",name);
+      window.location = "kwitter_page.html";
+}
